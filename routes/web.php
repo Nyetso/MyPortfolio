47566904af4ddb9ebef80d58a4890a353a568f14
Nyetso <harris_nazman@yahoo.com.sg>
2024-06-19 15:38:17 +0800

    controller setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'welcome']);
+Route::get('/about', 'AboutController@About');
+Route::get('/experience', 'ExperienceController@Experience');
+Route::get('/project', 'ProjectController@Project');
